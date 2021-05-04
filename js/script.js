9a764d0d549cb862035d57bd1298b01a1d450be3
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2021-05-04 15:55:33 +0200

    added favicon and functions in .js

--- js/script.js
+++ js/script.js
@@ -1,67 +1,70 @@
-console.log("Dzieńdoberek :)");
+{
+    const welcome = () => {
+        console.log("Welcome, how are you? :)");
+    };
 
-let form1 = document.querySelector(".js-form1");
-let diameterInput1 = document.querySelector(".js-diameterInput1");
-let rotationInput1 = document.querySelector(".js-rotationInput1");
-let speedResult1 = document.querySelector(".js-speedResult1");
+    const form1 = document.querySelector(".js-form1");
+    const form2 = document.querySelector(".js-form2");
+    const form3 = document.querySelector(".js-form3");
 
-form1.addEventListener("submit", (event) => {
-    event.preventDefault();
+    const form1submit = (event) => {
+        event.preventDefault();
+        const diameterInput1 = document.querySelector(".js-diameterInput1");
+        const rotationInput1 = document.querySelector(".js-rotationInput1");
+        let speedResult1 = document.querySelector(".js-speedResult1");
+        let speedElement;
+        speedElement = Math.PI * diameterInput1.value * rotationInput1.value / 1000;
+        speedResult1.value = speedElement.toFixed(2);
+    };
 
-    let speedElement;
+    const form2submit = (event) => {
+        event.preventDefault();
+        const diameterInput2 = document.querySelector(".js-diameterInput2");
+        const speedInput2 = document.querySelector(".js-speedInput2");
+        let rotationResult2 = document.querySelector(".js-rotationResult2");
+        let rotElement;
+        rotElement = speedInput2.value * 1000 / Math.PI / diameterInput2.value;
+        rotationResult2.value = rotElement.toFixed(2);
+    };
 
-    speedElement = Math.PI * diameterInput1.value * rotationInput1.value / 1000;
+    let feedElement;
 
-    speedResult1.value = speedElement.toFixed(2);
+    const form3input = (event) => {
+        const rotationInput3 = document.querySelector(".js-rotationInput3");
+        const feedForRev = document.querySelector(".js-feedForRev");
+        const feedForTooth = document.querySelector(".js-feedForTooth");
+        const toothNumber = document.querySelector(".js-toothNumber");
+        const rev = document.querySelector(".js-rev");
+        const tooth = document.querySelector(".js-tooth");
+        if (tooth.checked) {
+            feedElement = rotationInput3.value * feedForTooth.value * toothNumber.value;
+            feedForRev.disabled = true;
+            feedForRev.value = "";
+            feedForTooth.disabled = false;
+            toothNumber.disabled = false;
+        } else if (rev.checked) {
+            feedElement = rotationInput3.value * feedForRev.value;
+            feedForRev.disabled = false;
+            feedForTooth.disabled = true;
+            feedForTooth.value = "";
+            toothNumber.disabled = true;
+            toothNumber.value = "";
+        };
+    };
 
-})
+    const form3submit = (event) => {
+        event.preventDefault();
+        let feedResult3 = document.querySelector(".js-feedResult3");
+        feedResult3.value = feedElement;
+    };
 
-let form2 = document.querySelector(".js-form2");
-let diameterInput2 = document.querySelector(".js-diameterInput2");
-let speedInput2 = document.querySelector(".js-speedInput2");
-let rotationResult2 = document.querySelector(".js-rotationResult2");
+    const init = () => {
+        form1.addEventListener("submit", form1submit);
+        form2.addEventListener("submit", form2submit);
+        form3.addEventListener("input", form3input);
+        form3.addEventListener("submit", form3submit);
+    };
 
-form2.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    let rotElement;
-
-    rotElement = speedInput2.value * 1000 / Math.PI / diameterInput2.value;
-
-    rotationResult2.value = rotElement.toFixed(2);
-
-})
-
-let form3 = document.querySelector(".js-form3");
-let rotationInput3 = document.querySelector(".js-rotationInput3");
-let feedForRev = document.querySelector(".js-feedForRev");
-let feedForTooth = document.querySelector(".js-feedForTooth");
-let toothNumber = document.querySelector(".js-toothNumber");
-let feedResult3 = document.querySelector(".js-feedResult3");
-
-let rev = document.querySelector(".js-rev");
-let tooth = document.querySelector(".js-tooth");
-let feedElement;
-
-form3.addEventListener("input", (event) => {
-
-    if (tooth.checked) {
-        feedElement = rotationInput3.value * feedForTooth.value * toothNumber.value;
-        feedForRev.disabled = true;
-        feedForRev.value = "";
-        feedForTooth.disabled = false;
-        toothNumber.disabled = false;
-    } else if (rev.checked) {
-        feedElement = rotationInput3.value * feedForRev.value;
-        feedForRev.disabled = false;
-        feedForTooth.disabled = true;
-        feedForTooth.value = "";
-        toothNumber.disabled = true;
-        toothNumber.value = "";
-    }
-})
-
-form3.addEventListener("submit", (event) => {
-    event.preventDefault();
-    feedResult3.value = feedElement;
-})
+    welcome();
+    init();
+}
